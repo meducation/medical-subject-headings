eabf3b8484638001b78e5d624d909f862c1b47e1
Rob Styles <rob.styles@dynamicorange.com>
2014-03-06 16:21:50 +0000

    Minor addition to sibling test

--- test/mesh_test.rb
+++ test/mesh_test.rb
@@ -344,6 +344,7 @@ module MESH
       child2 = MESH::Mesh.find_by_tree_number('C19.053.500.270')
       child3 = MESH::Mesh.find_by_tree_number('C19.053.500.480')
       child4 = MESH::Mesh.find_by_tree_number('C19.053.500.740')
+      unrelated = MESH::Mesh.find('D008091')
       children = [child1, child2, child3, child4]
 
       children.each { |c| refute parent.sibling(c) }
@@ -351,6 +352,9 @@ module MESH
 
       children.each { |c| assert child1.sibling(c) unless c == child1 }
       children.each { |c| assert c.sibling(child1) unless c == child1 }
+
+      children.each { |c| refute unrelated.sibling(c) }
+      children.each { |c| refute c.sibling(unrelated) }
     end
 
     it 'should group headings into clusters with highest level first' do
